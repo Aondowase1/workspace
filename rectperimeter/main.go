b8f1e858c49a664f1b76c329a3af241a6bf646a0
Aondowase1 <amaaondowase63@gmail.com>
2025-10-22 17:06:14 +0100

    rectperimeter

---
+++ rectperimeter/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func RectPerimeter(w, h int) int {
+	if w < 0 || h < 0 {
+		return -1
+	}
+	return 2 * (w + h)
+}
+
+func main() {
+	fmt.Println(RectPerimeter(1 - 2))
+
+}
