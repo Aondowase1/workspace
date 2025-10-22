d772b20c003a44ae96e4937b2040b5aa1edb1ebf
Aondowase1 <amaaondowase63@gmail.com>
2025-10-22 14:43:35 +0100

    alphacount

---
+++ numbers/alphacount/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func AlphaCount(s string) int {
+	count := 0
+	for _, r := range s {
+        if (r := 'a' && r <= 'z') || (r := 'A' && r <= 'Z') {
+			count++
+		}
+	}
+	return count
+}
