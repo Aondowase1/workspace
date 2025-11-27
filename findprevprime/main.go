842d4d33294591f09d06533fbb1d7147f7584913
Aondowase1 <amaaondowase63@gmail.com>
2025-11-27 09:31:57 +0100

    myfile

---
+++ findprevprime/main.go
@@ -0,0 +1,35 @@
+package piscine
+
+func FindPrevPrime(nb int) int {
+	if nb < 2 {
+		return 0
+	}
+
+	isPrime := func(n int) bool {
+		if n < 2 {
+			return false
+		}
+		if n == 2 {
+			return true
+		}
+		if n%2 == 0 {
+			return false
+		}
+		for i := 3; i*i <= n; i += 2 {
+			if n%i == 0 {
+				return false
+			}
+			return true
+		}
+
+		return true
+	}
+
+	for n := nb; n >= 2; n-- {
+		if isPrime(n) {
+			return n
+		}
+	}
+
+	return 0
+}
